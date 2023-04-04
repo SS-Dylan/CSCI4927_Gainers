a5662dda747e0bac1a977bed1f3dfe212b6004ef
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2023-04-03 21:35:02 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSCI4927_Gainers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
